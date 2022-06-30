9e7691e27483664c76b9b4e92a400efbc13da859
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-06-30 15:00:12 -0300

    Delete jogo_memoria.js

--- jogo_memoria.js
+++
@@ -1,8 +0,0 @@
-const pecas = document.querySelectorAll('.col');
-
-function GirarPecas() {
-
-  this.classList.toggle('girar');
-}
-
-pecas.forEach(peca => peca.addEventListener('click', GirarPecas));
