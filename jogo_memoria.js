84d56559fbba4a416cbd03a04227c034f3f3d4e6
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-07-01 09:44:27 -0300

    Update jogo_memoria.js

--- jogo_memoria.js
+++ jogo_memoria.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
+const cards = document.querySelectorAll('.memory-card');
+var cartasClicadas = [];
 
-    flipCardsOnclick();
+function flipCard() {
+  this.classList.toggle('flip');
+  cartasClicadas.push(this);
+  if(cartasClicadas.length == 2){
+    jogoMemorial();
+  }
+}
 
-});
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+function jogoMemorial(){
+  if(cartasClicadas[0].src === cartasClicadas[1].src){
+    cartasClicadas.forEach(card => {
+      card.parentNode.removeChild(card);
+    });
+    cartasClicadas.lenght = 0;
+  }
+  else{
+    cartasClicadas.forEach(card => {
+      card.classList.toggle('flip');
+    });
+    cartasClicadas.lenght = 0;
+  }
+}
