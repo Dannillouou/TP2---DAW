e14207135285068ae24a849b903bdeb54373b566
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-07-01 11:12:38 -0300

    Add files via upload

--- jogo_memoria.js
+++ jogo_memoria.js
@@ -1,7 +1,39 @@
-const cards = document.querySelectorAll('.memory-card');
+function jogoDaMemoria(){
+  const cards = document.querySelectorAll('.memory-card');
+  
+  cards.forEach(card => {
+    if(card.classList.contains('flip'))
+      card.classList.toggle('flip');
+  });
 
-function flipCard() {
-  this.classList.toggle('flip');
+  var cartasClicadas = [];
+  
+  function flipCard() {
+    this.classList.toggle('flip');
+    cartasClicadas.push(this);
+    id = this.getAttribute('id');
+    console.log(id);
+    if(cartasClicadas.length == 2){
+      jogoMemorial();
+    }
+  }
+  
+  cards.forEach(card => card.addEventListener('click', flipCard));
+  
+  function jogoMemorial(){
+    let id1 = cartasClicadas[0].getAttribute('id');
+    let id2 = cartasClicadas[1].getAttribute('id');
+    if(id1 === id2){
+      cartasClicadas = [];
+    }
+    else{
+      setTimeout(() => { //delay de 1 segundo depois de clicar em cartas diferentes; tudo dentro de setTimeout demora um segundo para ser executado
+      cartasClicadas.forEach(card => {
+        card.classList.toggle('flip');
+      });
+      cartasClicadas = [];}, 500);
+    }
+  }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+jogoDaMemoria();
