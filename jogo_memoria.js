ab71213422bf6e2270fb8f6c6227116846209f83
Daniel Miranda <85716954+Dannillouou@users.noreply.github.com>
2022-07-01 10:29:34 -0300

    Update jogo_memoria.js

--- jogo_memoria.js
+++ jogo_memoria.js
@@ -1,29 +1,35 @@
-const cards = document.querySelectorAll('.memory-card');
-var cartasClicadas = [];
-
-function flipCard() {
-  this.classList.toggle('flip');
-  cartasClicadas.push(this);
-  id = this.getAttribute('id');
-  console.log(id);
-  if(cartasClicadas.length == 2){
-    jogoMemorial();
+function jogoDaMemoria(){
+  var cartasViradas = 0;
+  const cards = document.querySelectorAll('.memory-card');
+  var cartasClicadas = [];
+  
+  function flipCard() {
+    this.classList.toggle('flip');
+    cartasClicadas.push(this);
+    id = this.getAttribute('id');
+    console.log(id);
+    if(cartasClicadas.length == 2){
+      jogoMemorial();
+    }
+  }
+  
+  cards.forEach(card => card.addEventListener('click', flipCard));
+  
+  function jogoMemorial(){
+    let id1 = cartasClicadas[0].getAttribute('id');
+    let id2 = cartasClicadas[1].getAttribute('id');
+    if(id1 === id2){
+      cartasClicadas = [];
+      cartasViradas += 2;
+    }
+    else{
+      setTimeout(() => { //delay de 1 segundo depois de clicar em cartas diferentes; tudo dentro de setTimeout demora um segundo para ser executado
+      cartasClicadas.forEach(card => {
+        card.classList.toggle('flip');
+      });
+      cartasClicadas = [];}, 500);
+    }
   }
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-function jogoMemorial(){
-  let id1 = cartasClicadas[0].getAttribute('id');
-  let id2 = cartasClicadas[1].getAttribute('id');
-  if(id1 === id2){
-    cartasClicadas = [];
-  }
-  else{
-    setTimeout(() => { //delay de 1 segundo depois de clicar em cartas diferentes; tudo dentro de setTimeout demora um segundo para ser executado
-    cartasClicadas.forEach(card => {
-      card.classList.toggle('flip');
-    });
-    cartasClicadas = [];}, 1000);
-  }
-}
+jogoDaMemoria();
